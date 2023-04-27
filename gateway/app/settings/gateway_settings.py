0322f82a9248b96f821d0755bb58bb7100099ab7
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-04-27 13:20:34 -0300

    feat(gateway): add support for environment variables config

---
+++ gateway/app/settings/gateway_settings.py
@@ -0,0 +1,40 @@
+"""
+Gateway settings module.
+"""
+from pydantic import BaseSettings
+
+
+class GatewaySettings(BaseSettings):
+    """Define application configuration model.
+
+    Constructor will attempt to determine the values of any fields not passed
+    as keyword arguments by reading from the environment. Default values will
+    still be used if the matching environment variable is not set.
+
+    Environment variables:
+        * API_GATEWAY_AUTH_SERVICE_URL
+        * API_GATEWAY_ALLOCATION_SERVICE_URL
+        * API_GATEWAY_TIMEOUT
+
+    Attributes:
+        AUTH_SERVICE_URL (str): Auth service url.
+        ALLOCATION_SERVICE_URL (str): Allocation service url.
+        TIMEOUT (int): Timeout for requests.
+    """
+
+    AUTH_SERVICE_URL: str = "http://localhost:8000"
+    ALLOCATION_SERVICE_URL: str = "http://localhost:8001"
+    TIMEOUT: int = 59
+
+    class Config:
+        """Config subclass needed to customize BaseSettings settings.
+        Attributes:
+            case_sensitive (bool): When case_sensitive is True, the environment
+                variable names must match field names (optionally with a prefix)
+            env_prefix (str): The prefix for environment variable.
+        Resources:
+            https://pydantic-docs.helpmanual.io/usage/settings/
+        """
+
+        case_sensitive = True
+        env_prefix = "API_GATEWAY_"
