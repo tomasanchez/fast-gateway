96ac6b1062b709e21de1cd5ac094ac617417ad10
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-04-27 13:21:24 -0300

    refactor(actuator): update health response

--- gateway/app/domain/schemas.py
+++ gateway/app/domain/schemas.py
@@ -1,10 +1,11 @@
 """
 This module contains the schemas used for request/response validation in the allocation service.
 """
+from enum import Enum
 from re import sub
 from typing import Generic, TypeVar
 
-from pydantic import BaseConfig, BaseModel, Field
+from pydantic import BaseConfig, BaseModel, Field, EmailStr
 from pydantic.generics import GenericModel
 
 
@@ -52,8 +53,46 @@ class ResponseModels(GenericModel, Generic[T], CamelCaseModel):
     data: list[T] = Field(description="The response data list.")
 
 
+class ServiceHealthStatus(str, Enum):
+    """
+    The service health check status.
+
+    Attributes:
+        OK: The service is healthy.
+        ERROR: The service is unhealthy.
+        OFFLINE: The service is offline.
+    """
+    OK = "healthy"
+    ERROR = "unhealthy"
+    OFFLINE = "offline"
+
+
+class ServiceHealth(CamelCaseModel):
+    """
+    A response wrapper for a service health check.
+    """
+    name: str = Field(description="The service name.", example="auth service")
+    status: ServiceHealthStatus = Field(description="The health check status.", example="ok",
+                                        default=ServiceHealthStatus.OK)
+
+
 class HealthChecked(CamelCaseModel):
     """
     A response wrapper for a health check.
     """
-    status: str = Field(description="The health check status.", example="ok", default="ok")
+    status: ServiceHealthStatus = Field(description="The health check status.",
+                                        example="ok",
+                                        default=ServiceHealthStatus.OFFLINE)
+    redis: str = Field(description="The redis health check status.", example="offline", default="offline")
+    services: list[ServiceHealth] = Field(description="The services health check status.",
+                                          example=[ServiceHealth(name="auth service", status=ServiceHealthStatus.OK)],
+                                          default=list())
+
+
+class User(CamelCaseModel):
+    """
+    A user.
+    """
+    id: str = Field(description="The user id.", example="1")
+    username: str = Field(description="The username.", example="john")
+    email: EmailStr = Field(description="The email.", example="jd@e.mail")
--- gateway/app/entrypoints/actuator.py
+++ gateway/app/entrypoints/actuator.py
@@ -6,21 +6,47 @@ In essence, Actuator brings production-ready features to our application.
 Monitoring our app, gathering metrics, understanding traffic, or the state of our database become trivial with this
 dependency.
 """
+import asyncio
 
 from fastapi import APIRouter
-from starlette.status import HTTP_200_OK
+from starlette.responses import RedirectResponse
+from starlette.status import HTTP_200_OK, HTTP_301_MOVED_PERMANENTLY
 
-from app.domain.schemas import ResponseModel, HealthChecked
+from app.adapters.network import make_request
+from app.dependencies import AsyncHttpClientDependency
+from app.domain.schemas import ResponseModel, HealthChecked, ServiceHealth, ServiceHealthStatus
+from app.settings.gateway_settings import GatewaySettings
 
 router = APIRouter(tags=["Actuator"])
 
 
 @router.get("/health",
             status_code=HTTP_200_OK,
-            summary="Checks healthy upstream.",
-            tags=["actuator"], )
-async def health() -> ResponseModel[HealthChecked]:
+            summary="Checks healthy upstream.", )
+async def health(http_client: AsyncHttpClientDependency) -> ResponseModel[HealthChecked]:
     """
     Health check endpoint.
     """
-    return ResponseModel(data=HealthChecked())
+    settings = GatewaySettings()
+
+    try:
+        _, code = await make_request(url=settings.AUTH_SERVICE_URL + "/health", method="GET", client=http_client)
+        auth_service_status = ServiceHealthStatus.OK if code == HTTP_200_OK else ServiceHealthStatus.ERROR
+    except asyncio.TimeoutError:
+        auth_service_status = ServiceHealthStatus.OFFLINE
+
+    return ResponseModel(data=HealthChecked(
+        status=ServiceHealthStatus.OK,
+        services=[ServiceHealth(name="auth service", status=auth_service_status)]
+    ))
+
+
+@router.get("/",
+            status_code=HTTP_301_MOVED_PERMANENTLY,
+            include_in_schema=False,
+            summary="Redirects to health check endpoint.")
+async def root_redirect():
+    """
+    Redirects to health check endpoint.
+    """
+    return RedirectResponse(url="/docs")
